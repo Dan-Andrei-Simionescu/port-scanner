7c7b008a56efa70816c831a5a7281e1271c8d3a9
Dan-Andrei-Simionescu <andreidas04@gmail.com>
2026-07-22 23:49:12 +0300

    Add more arguments/glags, such as: timeout, workers, verbose, output

--- src/CL_arguments.py
+++ src/CL_arguments.py
@@ -1,8 +1,13 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="DasDuke Port Scanner")
-parser.add_argument("-t", dest="target", required=True, help="Target IP address")
-parser.add_argument("-p", dest="ports", default="1-1024", help="Port range to scan")
+parser.add_argument("-t", "--target", dest="target", required=True, help="Target IP address")
+parser.add_argument("-p", "--port", dest="ports", default="1-1024", help="Port range to scan")
+parser.add_argument("-o", "--output", dest="output", default=None, help="Save results to a file")
+parser.add_argument("-T", "--timeout", dest="timeout", default=0.5, type=float, help="Set TIMEOUT for current scan")
+parser.add_argument("-w", "--workers", dest="workers", default=100, type=int, help="Tune performance based on your machine/network power - Set numbers of port to be scanned simultaneously")
+parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Show closed ports too")
+
 args = parser.parse_args()
 
 parts = args.ports.split("-")
